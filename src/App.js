92412f1cfc1aa3320bdc03dcd8d004347b868c0a
fuadazizi <60860010+fuadazizi@users.noreply.github.com>
2022-07-08 18:17:12 +0700

    revamped

--- src/App.js
+++ src/App.js
@@ -24,72 +24,83 @@ import Error from './Error/Error'
 export default function App(params) {
   const { token, setToken } = useToken();
   // const { auth } = useContext(AuthContext);
-  // console.log(token);
-  let loginStatus = <> </>;
-  let logged = false;
-  if (token != undefined) {
-    console.log(token);
-    logged = true;
+  let authRouter = <> </>;
+  if (!token) {
+    authRouter = (
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login setToken={setToken} />} />
+        </Routes>
+      </Router>
+    )
+  } else {
+    authRouter = (
+      <Router>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    )
   }
-  // console.log(auth)
 
-  if (token === undefined) {
-    // console.log(Object.keys(auth).length)
-    return (
-      <div className="homepage-body">
-        <Router>
-          <ModalProvider>
-            <ScrollToTop />
-            <Routes>
-              <Route path="/" element={<Homepage />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login setToken={setToken} />} />
-            </Routes>
+  // if (!token) {
+  //   return (
+  //     <div className="homepage-body">
+  //       <Router>
+  //         <ModalProvider>
+  //           <ScrollToTop />
+  //           <Routes>
+  //             <Route path="/" element={<Homepage />} />
+  //             <Route path="/register" element={<Register />} />
+  //             <Route path="/login" element={<Login setToken={setToken} />} />
+  //           </Routes>
 
-            <Routes>
-              <Route
-                path="/product"
-                element={<Product variant="furniture" />}
-              />
-              <Route
-                path="/product/:productId"
-                element={<ProductDetailed />}
-              />
-              <Route
-                path="/product/room-page"
-                element={<RoomDetailed />}
-              />
+  //           <Routes>
+  //             <Route
+  //               path="/product"
+  //               element={<Product variant="furniture" />}
+  //             />
+  //             <Route
+  //               path="/product/:productId"
+  //               element={<ProductDetailed />}
+  //             />
+  //             <Route
+  //               path="/product/room-page"
+  //               element={<RoomDetailed />}
+  //             />
 
-              <Route
-                path="/ruangan"
-                element={<Product variant="room" />}
-              />
+  //             <Route
+  //               path="/ruangan"
+  //               element={<Product variant="room" />}
+  //             />
 
-              {/* <Route path="/furnitur/product-page" element={<RoomPage />} />  */}
-              <Route path="/canvas" element={<Canvas />} />
-              <Route path="/consult" element={<Consult />} />
+  //             {/* <Route path="/furnitur/product-page" element={<RoomPage />} />  */}
+  //             <Route path="/canvas" element={<Canvas />} />
+  //             <Route path="/consult" element={<Consult />} />
 
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/checkout" element={<Checkout />} />
+  //             <Route path="/cart" element={<Cart />} />
+  //             <Route path="/checkout" element={<Checkout />} />
 
-              <Route path="/invoice" element={<Invoice />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route
-                path="/forget-password"
-                element={<ForgetPassword />}
-              />
-              
-            {/* Check if the requested url error */}
-            <Route element={<Error />} />
-            </Routes>
-          </ModalProvider>
-        </Router>
-      </div>
-    );
-  }
+  //             <Route path="/invoice" element={<Invoice />} />
+  //             <Route path="/profile" element={<Profile />} />
+  //             <Route
+  //               path="/forget-password"
+  //               element={<ForgetPassword />}
+  //             />
+
+  //             {/* Check if the requested url error */}
+  //             <Route element={<Error />} />
+  //           </Routes>
+  //         </ModalProvider>
+  //       </Router>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="homepage-body">
+      {authRouter}
       <Router>
         <ModalProvider>
           <ScrollToTop />
